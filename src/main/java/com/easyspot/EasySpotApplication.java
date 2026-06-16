74ab7e836eff8f533791acde4863e16b5acb1cad
Julian Quesada Víquez <julian.quesadaviquez@eagles.oc.edu>
2026-06-16 17:05:39 -0600

    Commit #1: Inicialización del Proyecto - EasySpot

---
+++ src/main/java/com/easyspot/EasySpotApplication.java
@@ -0,0 +1,13 @@
+package com.easyspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasySpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasySpotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easyspot/EasySpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasySpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
